a038f51a233760913eace89eed1772e71c5b618f
lixin <lixin@jkweixin.com>
2018-05-09 14:26:05 +0800

    首页页面UI基本完成,除了动画等

--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -39,14 +39,53 @@
       </ul>
     </header>
     <footer class="tab-leader" v-if="userId===0">
-      <nav class="t-l-nav" :style="leaderImg">联盟管理
-        <!--<img class="t-l-n-bg" v-if="imageUri" :src="imageUri +'/defaults/ipc-shopping/home/icon-mhome_union@2x.png)'">-->
-      </nav>
-      <nav class="t-l-nav" :style="activeImg">活动管理
-        <!--<img class="t-l-n-bg" v-if="imageUri" :src="imageUri +'/defaults/ipc-shopping/home/icon-mhome_union@2x.png)'">-->
-      </nav>
-      <nav class="t-l-nav" :style="employeeImg">员工管理
-      </nav>
+      <nav class="t-l-nav" :style="leaderImg">联盟管理</nav>
+      <nav class="t-l-nav" :style="activeImg">活动管理</nav>
+      <nav class="t-l-nav" :style="employeeImg">员工管理</nav>
+      <nav class="t-l-nav" :style="incomeImg">收入/提现</nav>
+    </footer>
+    <footer class="tab-merchant" v-if="userId===1">
+      <section class="t-m-ad">
+        <div class="title">公告</div>
+        <div class="content">异业联盟卡活动正在等你报名正在等你…</div>
+        <div class="watch-btn">查看</div>
+      </section>
+      <nav class="t-l-nav" :style="activeImg">活动管理</nav>
+      <nav class="t-l-nav" :style="employeeImg">员工管理</nav>
+      <nav class="t-l-nav" :style="incomeImg">收入/提现</nav>
+    </footer>
+    <footer class="tab-employee" v-if="userId===2">
+      <section class="t-e-sales">
+        <div class="title">销卡对比</div>
+        <article class="content-box">
+          <scroll-view class="content" scroll-y="true">
+            <ul class="c-holder">
+              <li class="emp-item" v-for="(item,index) in employeeList" :key="index">
+                <article class="emp-item-box">
+                  <section class="icon">
+                    <img class="icon-pic" v-if="imageUri" :src="imageUri+'/defaults/ipc-shopping/home/icon-mhome_union@2x.png'">
+                  </section>
+                  <section class="progress">
+                    <div class="name-bar">
+                      <div class="name">陈蒙蒙meo</div>
+                      <div class="sales">8/10</div>
+                    </div>
+                    <article class="p-box">
+                      <div class="p-pro"></div>
+                    </article>
+                  </section>
+                  <section class="number">
+                    <div class="n-title">销卡</div>
+                    <div>80</div>
+                    <div>%</div>
+                  </section>
+                </article>
+              </li>
+            </ul>
+          </scroll-view>
+        </article>
+      </section>
+      <nav class="t-l-nav" :style="activeImg">活动管理</nav>
       <nav class="t-l-nav" :style="incomeImg">收入/提现</nav>
     </footer>
   </div>
@@ -77,38 +116,75 @@
   }
 
   export default {
-    data() {
+    data () {
       return {
         imageUri: api.image,
         activeList: new Array(3).fill(object),
-        swiperCurrent: 0,
+        employeeList: new Array(6).fill(0),
         dotCurrent: 0,
         userId: 0
       }
     },
     methods: {
-      swiperChange(e) {
+      swiperChange (e) {
         console.log(e)
         let index = e.mp.detail.current
         this.dotCurrent = index
-        this.swiperCurrent = index
+        this.userId = index
+        // this.swiperCurrent = index
       }
     },
     computed: {
-      arrowImg() {
+      arrowImg () {
         return `background-image:url(${this.imageUri}/defaults/ipc-shopping/home/icon-mhome_j@2x.png)` || ''
       },
-      leaderImg() {
+      leaderImg () {
         return `background-image:url(${this.imageUri}/defaults/ipc-shopping/home/icon-mhome_union@2x.png)` || ''
       },
-      activeImg() {
-        return `background-image:url(${this.imageUri}/defaults/ipc-shopping/home/icon-mhome_activity@2x.png)` || ''
+      activeImg () {
+        switch (this.userId) {
+          case 0: {
+            return `background-image:url(${this.imageUri}/defaults/ipc-shopping/home/icon-mhome_activity@2x.png)` || ''
+          }
+          case 1: {
+            return `background-image:url(${this.imageUri}/defaults/ipc-shopping/home/icon-shome_activity@2x.png)` || ''
+          }
+          case 2: {
+            return `background-image:url(${this.imageUri}/defaults/ipc-shopping/home/icon-yhome_activity@2x.png)` || ''
+          }
+          default: {
+            return ''
+          }
+        }
       },
-      employeeImg() {
-        return `background-image:url(${this.imageUri}/defaults/ipc-shopping/home/icon-mhome_staff@2x.png)` || ''
+      employeeImg () {
+        switch (this.userId) {
+          case 0: {
+            return `background-image:url(${this.imageUri}/defaults/ipc-shopping/home/icon-mhome_staff@2x.png)` || ''
+          }
+          case 1: {
+            return `background-image:url(${this.imageUri}/defaults/ipc-shopping/home/icon-shome_staff@2x.png)` || ''
+          }
+          default: {
+            return ''
+          }
+        }
       },
-      incomeImg() {
-        return `background-image:url(${this.imageUri}/defaults/ipc-shopping/home/icon-mhome_income@2x.png)` || ''
+      incomeImg () {
+        switch (this.userId) {
+          case 0: {
+            return `background-image:url(${this.imageUri}/defaults/ipc-shopping/home/icon-mhome_income@2x.png)` || ''
+          }
+          case 1: {
+            return `background-image:url(${this.imageUri}/defaults/ipc-shopping/home/icon-shome_income@2x.png)` || ''
+          }
+          case 2: {
+            return `background-image:url(${this.imageUri}/defaults/ipc-shopping/home/icon-yhome_income@2x.png)` || ''
+          }
+          default: {
+            return ''
+          }
+        }
       }
     }
   }
@@ -125,17 +201,17 @@
       .swiper-box
         margin: 0 4%
         height: 0
-        padding-top :78.4%
+        padding-top: 78.4%
         background: $color-assist-27
         box-shadow: 0 2px 20px 0 rgba(0, 0, 0, 0.15)
         border-radius: 3px
         position: relative
         .s-b-box
-          position :absolute
-          top:0
-          bottom :0
-          right :0
-          left :0
+          position: absolute
+          top: 0
+          bottom: 0
+          right: 0
+          left: 0
           .home-swiper
             height: 100%
             width: 100%
@@ -186,7 +262,7 @@
                     font-size: $font-size-large-xx
                     color: $color-background-ff
                     min-width: 60px
-                    max-width :100px
+                    max-width: 100px
                     no-wrap()
                   .title
                     margin-top: 1.7766497461928%
@@ -223,7 +299,7 @@
       layout(row)
       justify-content: space-between
       .t-l-nav
-        padding :8.845% 0
+        padding: 8.845% 0
         width: 47.826086%
         background-size: 100%
         background-position: center center
@@ -235,4 +311,170 @@
         border-radius: 3px
         &:nth-child(3), &:nth-child(4)
           margin-top: 4.133333%
+
+    .tab-merchant
+      position: relative
+      padding: 4.8% 4% 0
+      layout(row)
+      justify-content: space-between
+      .t-m-ad
+        position: relative
+        padding: 0 0.5px
+        box-sizing: border-box
+        width: 100%
+        margin-bottom: 6.6666666666%
+        layout(row, block, no-wrap)
+        justify-content: space-between
+        .title
+          background: $color-assist-34
+          border-radius: 3px
+          width: 40.5px
+          height: 21px
+          font-family: $font-family-meddle
+          font-size: $font-size-medium
+          color: $color-background-ff
+          text-align: center
+          line-height: 21px
+        .content
+          flex: 1
+          padding-left: 5px
+          height: 21px
+          font-family: $font-family-light
+          font-size: $font-size-medium
+          color: $color-background-ff
+          line-height: 21px
+          no-wrap()
+        .watch-btn
+          width: 47px
+          height: 21px
+          font-family: $font-family-light
+          font-size: $font-size-small
+          color: $color-assist-34
+          text-align: right
+          line-height: 21px
+      .t-l-nav
+        padding: 10.43478260869% 0 20.579710144927%
+        width: 31.304347826086%
+        background-size: 100%
+        background-position: center center
+        background-repeat: no-repeat
+        font-family: $font-family-regular
+        font-size: $font-size-medium-x
+        color: $color-background-ff
+        text-align: center
+        border-radius: 3px
+
+    .tab-employee
+      position: relative
+      padding: 2.6666666666% 4% 0
+      layout(row)
+      justify-content: space-between
+      .t-e-sales
+        width: 100%
+        box-sizing: border-box
+        background: $color-assist-27
+        box-shadow: 0 2px 20px 0 rgba(0, 0, 0, 0.15)
+        border-radius: 3px
+        margin-bottom: 4%
+        .title
+          padding: 0 4.34782608695%
+          height: 45px
+          line-height: 45px
+          font-family: $font-family-regular
+          font-size: $font-size-medium-x
+          color: $color-background-ff
+          cut-off-rule-bottom(4.34782608695%, 0, $color-row-line-3f)
+        .content-box
+          position: relative
+          height: 0
+          padding-top: 37.971014492753%
+          .content
+            position: absolute
+            top: 0
+            left: 0
+            right: 0
+            bottom: 0
+            padding-bottom: 4.8%
+            .c-holder
+              position: relative
+              padding-top: 3.7681159420289%
+              overflow: hidden
+              .emp-item
+                position: relative
+                height: 0
+                padding-top: 6.9565217391304%
+                margin-bottom: 3.9130434782608%
+                &:last-child
+                  margin-bottom: 0
+                .emp-item-box
+                  fill-box()
+                  layout(row)
+                  padding: 0 4.3478260869565%
+                  justify-content: space-between
+                  align-items: center
+                  .icon
+                    width: 24px
+                    height: 24px
+                    box-sizing: border-box
+                    border-radius: 50%
+                    overflow: hidden
+                    .icon-pic
+                      width: 100%
+                      height: 100%
+                  .progress
+                    position: relative
+                    flex: 1
+                    padding: 0 24px 0 15px
+                    layout()
+                    justify-content: space-between
+                    .name-bar
+                      flex: 1
+                      layout(row)
+                      justify-content: space-between
+                      align-items: center
+                      .name
+                        display: inline-block
+                        font-family: $font-family-light
+                        font-size: $font-size-small
+                        color: $color-background-ff
+                        text-indent: 1px
+                      .sales
+                        display: inline-block
+                        font-family: $font-family-din
+                        font-size: $font-size-small-s
+                        color: $color-text-95
+                    .p-box
+                      height: 6px
+                      background: $color-pro-bg-1f
+                      border-radius: 100px
+                      overflow: hidden
+                      .p-pro
+                        width: 50%
+                        height: 100%
+                        background-image: linear-gradient(-90deg, $color-assist-2a 0%, $color-assist-33 100%)
+                        border-radius: 100px
+                  .number
+                    position: relative
+                    layout(row, block, no-wrap)
+                    align-items: center
+                    font-family: $font-family-light
+                    font-size: $font-size-small-s
+                    color: $color-background-ff
+                    div:last-child
+                      font-size: $font-size-small-ss
+                    div:nth-child(2)
+                      font-family: $font-family-din
+                      font-size: $font-size-large
+
+      .t-l-nav
+        padding: 4.927536231884% 0
+        width: 47.826086956521%
+        background-size: 100%
+        background-position: center center
+        background-repeat: no-repeat
+        font-family: $font-family-regular
+        font-size: $font-size-medium-x
+        color: $color-background-ff
+        text-align: center
+        border-radius: 3px
 </style>
