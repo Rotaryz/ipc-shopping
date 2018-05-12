48bb58f4b02d83f60a49c2637c1bd923f8c00d5d
lixin <lixin@jkweixin.com>
2018-05-12 16:57:20 +0800

    优惠券组件完成

---
+++ src/components/check-status-bar-item/check-status-bar-item.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="check-status-bar-item">
+
+    </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    name: ''
+  }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "../../common/stylus/variable.styl"
+  @import "../../common/stylus/mixin.styl"
+
+</style>
