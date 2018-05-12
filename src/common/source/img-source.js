521885569f6ffc54123d46febe70dde2b883931c
lixin <lixin@jkweixin.com>
2018-05-12 17:42:52 +0800

    保存代码

--- src/common/source/img-source.js
+++ src/common/source/img-source.js
@@ -4,7 +4,7 @@ const baseUrl = `${api.image}/defaults/ipc-shopping`
 
 export default {
   /**
-   * 向右箭头图片
+   * 向右箭头图片-白色-FF
    * @param type 类型：默认背景图
    * @returns {*}
    */
@@ -12,6 +12,24 @@ export default {
     switch (type) {
       case 'background': {
         return `background-image:url(${baseUrl}/home/icon-mhome_j@2x.png)` || ''
+      }
+      default :
+        return ''
+    }
+  },
+  /**
+   * 向右箭头图片-灰色-A4
+   * @param type
+   * @returns {*}
+   */
+  imgArrowRightA4(type = 'background') {
+    const img = `icon-union_jt2@2x.png`
+    switch (type) {
+      case 'background': {
+        return `background-image:url(${baseUrl}/common/${img}` || ''
+      }
+      case 'img': {
+        return `${baseUrl}/common/${img}` || ''
       }
       default :
         return ''
@@ -26,6 +44,21 @@ export default {
     switch (type) {
       case 'background': {
         return `background-image:url(${baseUrl}/aliance/icon-union_card@2x.png)` || ''
+      }
+      default :
+        return ''
+    }
+  },
+  /**
+   * map-icon图标
+   * @param type
+   * @returns {*}
+   */
+  imgMapIcon(type = 'background') {
+    const img = `icon-union_map@2x.png`
+    switch (type) {
+      case 'background': {
+        return `background-image:url(${baseUrl}/common/${img}` || ''
       }
       default :
         return ''
