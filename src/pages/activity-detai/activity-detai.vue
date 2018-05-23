d2666b99989dc6fbdeded23596dac0835d39482a
liuhonglan <liuhonglan@jkweixin.com>
2018-05-23 14:39:50 +0800

    活动详情样式

--- src/pages/activity-detai/activity-detai.vue
+++ src/pages/activity-detai/activity-detai.vue
@@ -162,7 +162,7 @@
         font-size: $font-size-medium
         color: $color-text-46
         padding-left: 8px
-        vertical-line()
+        vertical-line($color-ktv-theme)
       .modal-item
         height: 100px
         display: flex
